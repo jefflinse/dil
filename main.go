f32964b90e31a6d4cc287759dc3383d82c35aa89
Jeff Linse <jeff@jefflinse.com>
2023-09-10 18:57:53 -0700

    Results of ChatGPT 4

--- main.go
+++ main.go
@@ -9,18 +9,30 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	var cmd = &cobra.Command{
 		Use:   "linter [path]",
-		Short: "Linter to detect external package usages",
+		Short: "Linter to detect significant external package usages",
 		Run:   runLinter,
 		Args:  cobra.ExactArgs(1),
 	}
 
+	setupConfig()
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func setupConfig() {
+	viper.SetConfigName(".didetect")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file, %s", err)
 	}
 }
 
@@ -32,12 +44,23 @@ func runLinter(cmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 
+	allowedStdLibs := viper.GetStringSlice("allowed_std_libs")
+	allowedLibsMap := make(map[string]struct{}, len(allowedStdLibs))
+	for _, lib := range allowedStdLibs {
+		allowedLibsMap[lib] = struct{}{}
+	}
+
 	for _, pkg := range pkgs {
 		for fileName, file := range pkg.Files {
 			ast.Inspect(file, func(n ast.Node) bool {
 				if call, ok := n.(*ast.CallExpr); ok {
 					if sel, isSelExpr := call.Fun.(*ast.SelectorExpr); isSelExpr {
 						if x, isIdent := sel.X.(*ast.Ident); isIdent {
+							// Ignore trivial packages from the standard library
+							if _, isTrivial := allowedLibsMap[x.Name]; isTrivial {
+								return true
+							}
+
 							// Checking if the package being used is not the current package
 							if !strings.EqualFold(x.Name, pkg.Name) {
 								fmt.Printf("External package %s used in file: %s, line: %d\n", x.Name, fileName, fs.Position(call.Pos()).Line)
